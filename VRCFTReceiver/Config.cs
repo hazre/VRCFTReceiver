adb52f8ccba37098f6a5ddb2cff77b8fe50825ef
hazre <37149950+hazre@users.noreply.github.com>
2024-11-28 02:04:10 +0100

    config string changes

--- VRCFTReceiver/Config.cs
+++ VRCFTReceiver/Config.cs
@@ -6,9 +6,9 @@ namespace VRCFTReceiver;
 public partial class VRCFTReceiver : ResoniteMod
 {
     [AutoRegisterConfigKey]
-    internal static ModConfigurationKey<bool> Enabled_Config = new("Enabled", "When checked, enables face & eye tracking", () => true);
+    internal static ModConfigurationKey<bool> Enabled_Config = new("Enabled", "Enable face & eye tracking?", () => true);
     public static bool Enabled => Config!.GetValue(Enabled_Config);
 
     [AutoRegisterConfigKey]
-    internal static ModConfigurationKey<int> Port_Config = new("Port", "The port on which OSC will listen", () => 9000);
+    internal static ModConfigurationKey<int> Port_Config = new("Port", "Which port should the OSC data be received from?", () => 9000);
 }
